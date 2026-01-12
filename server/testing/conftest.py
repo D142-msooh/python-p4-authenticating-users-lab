610db720da57045892d33bbcce1bbd2c14c1979a
D142-msooh <kinyajuidama176@gmail.com>
2026-01-12 10:22:05 +0300

    Add all files

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import pytest
+from app import app
+from models import db, User
+
 def pytest_itemcollected(item):
     par = item.parent.obj
     node = item.obj
@@ -7,3 +11,21 @@ def pytest_itemcollected(item):
     suf = node.__doc__.strip() if node.__doc__ else node.__name__
     if pref or suf:
         item._nodeid = ' '.join((pref, suf))
+
+@pytest.fixture
+def client():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['TESTING'] = True
+
+    with app.app_context():
+        db.create_all()
+        
+        # Create a test user
+        user = User(username='testuser')
+        db.session.add(user)
+        db.session.commit()
+        
+        yield app.test_client()
+        
+        db.session.remove()
+        db.drop_all()
